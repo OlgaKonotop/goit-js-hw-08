5a0a8144123d53ab337174a29936c8db1481c360
OlgaKonotop <konotop-olga@ukr.net>
2022-11-06 18:52:36 +0200

    hw08-test

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,24 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryBox = document.querySelector('.gallery');
+
+//console.log(galleryBox)
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) => ` 
+<a class="gallery__item" href="${original}" > 
+<img class="gallery__image" src="${preview}" alt="${description}" /> 
+</a>`
+  )
+  .join('');
+//console.log(markup);
+galleryBox.insertAdjacentHTML('beforeend', markup);
+//console.log(SimpleLightbox)
+
+const gallerySimpleLightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+console.log(gallerySimpleLightbox);
